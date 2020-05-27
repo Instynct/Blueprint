1521acbf487411d4152e160a02f50ea246613b38
Jonathan Barbeau <jonathan@instynctweb.com>
2020-05-27 14:27:04 -0400

    cleaned up functions.php

--- app/public/wp-content/themes/blueprint-instynct/header.php
+++ app/public/wp-content/themes/blueprint-instynct/header.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,11 +8,11 @@
     <link href="//www.google-analytics.com" rel="dns-prefetch">
     <link rel="profile" href="http://gmpg.org/xfn/11">
 
-    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url')?>/dist/css/main.min.css" async/>
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url') ?>/dist/css/main.min.css" async />
 
     <script src="https://kit.fontawesome.com/b78479514c.js" crossorigin="anonymous"></script>
 
-    <script src="<?php bloginfo('template_url')?>/dist/js/components.min.js" async></script>
+    <script src="<?php bloginfo('template_url') ?>/dist/js/components.min.js" async></script>
 
     <?php wp_head(); ?>
 </head>
