ffe8d9f12107ea64dbc77f47a9c51d19656957dd
Jonathan Barbeau <jonathan@instynctweb.com>
2020-07-03 14:32:03 -0400

    removed shortcodes and utilities

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 // Utilities
 
-include('inc/utilities.php');
+//include('inc/utilities.php');
 
 // CONFIG
 
@@ -11,10 +11,6 @@ include('inc/configure.php');
 // JAVASCRIPT & CSS
 
 include('inc/js-css.php');
-
-// SHORTCODES
-
-include('inc/shortcodes.php');
 
 // ACF
 
@@ -33,29 +29,3 @@ require_once('inc/custom-post-types.php');
 
 // Tiny MCE Styles
 require_once('inc/tiny-mce.php');
-
-// Disable WordPress Admin Bar for all users but admins. //
-show_admin_bar(false);
-
-if (is_admin()) {
-	include('inc/admin.php');
-}
-
-// Removing unecessary admin menu tabs
-function custom_menu_page_removing() {
-	remove_menu_page('edit-comments.php');
-}
-
-// Showing Menus in Appearance
-function register_my_menus() {
-	register_nav_menus();
-}
-
-function remove_editor() {
-	remove_post_type_support('page', 'editor');
-}
-add_action('admin_init', 'remove_editor');
-add_filter('use_block_editor_for_post_type', '__return_false');
-add_action('admin_menu', 'custom_menu_page_removing');
-add_action('init', 'register_my_menus');
-add_action('login_enqueue_scripts', 'custom_login_demarrage');
