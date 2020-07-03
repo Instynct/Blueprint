aff1735214ae8e61c28080ec7a8c2af3e5ea7b62
Jonathan Barbeau <jonathan@instynctweb.com>
2020-07-03 14:34:08 -0400

    removed shortcodes files and changed configure

--- inc/configure.php
+++ inc/configure.php
@@ -1,85 +1,31 @@
 <?php
+// Disable WordPress Admin Bar for all users but admins. //
+show_admin_bar(false);
 
-// MENUS
-function _custom_theme_register_menu()
-{
-    register_nav_menus(
-        array(
-            'menu-main' => __( 'Menu principal' ),
-            //'menu-footer' => __( 'Menu footer' ),
-        )
-    );
+if (is_admin()) {
+	include('inc/admin.php');
 }
-add_action( 'init', '_custom_theme_register_menu' );
 
-function custom_setup() {
-	// IMAGES
-	add_theme_support( 'post-thumbnails' );
-
-	// TITLE TAGS
-	add_theme_support('title-tag');
-
-	// LANGUAGES
-	load_theme_textdomain('textdomaintomodify', get_template_directory() . '/languages');
-
-	// HTML 5 - Example : deletes type="*" in scripts and style tags
-	add_theme_support( 'html5', [ 'script', 'style' ] );
-
-	// CUSTOM IMAGE SIZES
-	// add_image_size( '424x424', 424, 424, true );
-	// add_image_size( '1920', 1920, 9999 );
+// Removing unecessary admin menu tabs
+function custom_menu_page_removing() {
+	remove_menu_page('edit-comments.php');
 }
-add_action('after_setup_theme', 'custom_setup');
 
-// Giving credits
-function remove_footer_admin () {
-	echo 'Thème crée par <a href="http://www.olivier-guilleux.com" target="_blank">Olivier Guilleux</a>';
+// Showing Menus in Appearance
+function register_my_menus() {
+	register_nav_menus(
+		array(
+			'navigation' => __('Navigation'),
+			'navigation-footer' => __('Navigation Footer')
+		)
+	);
 }
-add_filter('admin_footer_text', 'remove_footer_admin');
 
-// Move Yoast to bottom
-function yoasttobottom() {
-	return 'low';
+function remove_editor() {
+	remove_post_type_support('page', 'editor');
 }
-add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
-
-// Remove WP Emoji
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
-
-// delete wp-embed.js from footer
-function my_deregister_scripts(){
-	wp_deregister_script( 'wp-embed' );
-}
-add_action( 'wp_footer', 'my_deregister_scripts' );
-
-// delete jquery migrate
-function dequeue_jquery_migrate( &$scripts){
-	if(!is_admin()){
-		$scripts->remove( 'jquery');
-		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4', true );
-	}
-}
-add_filter( 'wp_default_scripts', 'dequeue_jquery_migrate' );
-
-// force all scripts to load in footer
-function clean_header() {
-	remove_action('wp_head', 'wp_print_scripts');
-	remove_action('wp_head', 'wp_print_head_scripts', 9);
-	remove_action('wp_head', 'wp_enqueue_scripts', 1);
-}
-add_action('wp_enqueue_scripts', 'clean_header');
-
-// add SVG to allowed file uploads
-function add_file_types_to_uploads($mime_types){
-	$mime_types['svg'] = 'image/svg+xml';
-
-	return $mime_types;
-}
-add_action('upload_mimes', 'add_file_types_to_uploads', 1, 1);
-
-// disabling big image sizes scaled
-add_filter( 'big_image_size_threshold', '__return_false' );
+add_action('admin_init', 'remove_editor');
+add_filter('use_block_editor_for_post_type', '__return_false');
+add_action('admin_menu', 'custom_menu_page_removing');
+add_action('init', 'register_my_menus');
+add_action('login_enqueue_scripts', 'custom_login_demarrage');
