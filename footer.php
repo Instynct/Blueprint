cb2d7f3a9ce00a86ac8ade4b95df585aeec4ceb8
Jonathan Barbeau <jonathan@instynctweb.com>
2020-12-09 17:53:57 -0500

    new blueprint version

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@ $copyright = get_field('copyright', 'options');
 <footer class="footer" id="contact">
     <div class="footer__container container">
         <div class="footer__top">
-            <div class="logo"><img src="<?= $footerLogo['url'] ?>" alt="<?= $footerLogo['title'] ?>"></div>
+            <div class="footer__logo"><img src="<?= $footerLogo['url'] ?>" alt="<?= $footerLogo['title'] ?>"></div>
         </div>
-        <div class="copyright">
+        <div class="footer__copyright">
             <p><?= $copyright ?> <a href="https://instynctweb.com/">Instynct</a></p>
         </div>
     </div>
--- tp-home.php
+++ tp-home.php
@@ -6,6 +6,13 @@ Template Name: Home
 // Load header.php
 get_header(); ?>
 
+<section class="home">
+    <div class="home__container container">
+        <h1 class="home__title">Instynct Blueprint V2.5</h1>
+    </div>
+
+</section>
+
 <?php
 // Load footer.php
 get_footer(); ?>
