964188a6d5b4e409a4d9a342809b6810888b6596
Jonathan Barbeau <jonathan@instynctweb.com>
2021-04-08 10:11:04 -0400

    installed loco-scroll

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,18 +1,13 @@
+import LocomotiveScroll from 'locomotive-scroll';
 import Utils from './_utils';
-import SmoothScrollbar from './_wolfpack';
 
 const App = {
 	/**
 	 * App.init
 	 */
-	init(wolfpack) {
+	init() {
 		// Utils Scripts
 		const utils = new Utils();
-
-		let scrollBar = '';
-		scrollBar = wolfpack.scrollbar;
-		scrollBar.options.speed = 0.01;
-		scrollBar.options.damping = 0.1;
 
 		// Calculate True 100vh for Mobile on selected element
 		const rootElements = document.querySelectorAll('[data-calc-mobile]');
@@ -21,6 +16,11 @@ const App = {
 				utils.calculateHeight(rootElements[i]);
 			}
 		}
+
+		const scroll = new LocomotiveScroll({
+			el: document.querySelector('[data-wolfpack]'),
+			smooth: true,
+		});
 
 		// Resize functions
 		const scrollContainer = document.querySelector('[data-wolfpack]');
@@ -37,11 +37,5 @@ const App = {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-	const scrollContainer = document.querySelector('[data-wolfpack]');
-	let wolfpack = false;
-	if (scrollContainer) {
-		wolfpack = new SmoothScrollbar(scrollContainer);
-		wolfpack.init();
-	}
-	App.init(wolfpack);
+	App.init();
 });
