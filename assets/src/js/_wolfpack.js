618d8f68976bc27c4c0fc0e60be04ba8525e64f8
Jonathan Barbeau <jonathan@instynctweb.com>
2021-05-31 14:11:40 -0400

    added plugin syntax

--- assets/src/js/_wolfpack.js
+++ assets/src/js/_wolfpack.js
@@ -3,7 +3,19 @@ import Scrollbar from 'smooth-scrollbar';
 import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
 import TweenMax from 'gsap';
 
-Scrollbar.use(OverscrollPlugin);
+class NavPlugin extends ScrollbarPlugin {
+	static pluginName = 'modal';
+
+	static defaultOptions = {
+		open: false,
+	};
+
+	transformDelta(delta) {
+		return this.options.open ? { x: 0, y: 0 } : delta;
+	}
+}
+
+Scrollbar.use(NavPlugin, OverscrollPlugin);
 
 export default class SmoothScrollbar {
 	constructor(scrollContainer) {
@@ -23,6 +35,7 @@ export default class SmoothScrollbar {
 
 		this.scrollbar.track.xAxis.element.remove();
 		const scrollbar = this.scrollbar;
+
 		// MARQUEE
 		const marqueeElements = document.querySelectorAll('[data-marquee]');
 		if (marqueeElements.length > 0) {
